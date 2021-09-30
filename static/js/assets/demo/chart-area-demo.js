27dd67f201832c9f65418533ecc9b087babea26e
vijaygrg27 <77456706+vijaygrg27@users.noreply.github.com>
2021-09-30 16:23:11 +0530

    Add files via upload

---
+++ static/js/assets/demo/chart-area-demo.js
@@ -0,0 +1,165 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+var csv_file = "static/csv/IN0666B.csv"
+function load_csv(parm1) {
+str1 = "static/csv/",
+csv_file1 = str1 + district[String(parm1)];
+} 
+d3.csv(csv_file)
+.then(makeChart);
+// Area Chart Example
+function makeChart(weather) {
+    var weatherdate = weather.map(function(d) {return d.date});
+    var temp = weather.map(function(d) {return d.temp});
+    var percp = weather.map(function(d) {return d.prcp});
+    var maxspeed = weather.map(function(d) {return d.mxspd});
+    var ctx = document.getElementById("temp");
+    var myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: weatherdate,
+        datasets: [{
+          label: "Sessions",
+          lineTension: 0.3,
+          backgroundColor: "rgba(2,117,216,0.2)",
+          borderColor: "rgba(2,117,216,1)",
+          pointRadius: 5,
+          pointBackgroundColor: "rgba(2,117,216,1)",
+          pointBorderColor: "rgba(255,255,255,0.8)",
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(2,117,216,1)",
+          pointHitRadius: 50,
+          pointBorderWidth: 2,
+          data: temp
+        }],
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            time: {
+              unit: 'date'
+            },
+            gridLines: {
+              display: false
+            },
+            ticks: {
+              maxTicksLimit: 20
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: 40,
+              maxTicksLimit: 10
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, .125)",
+            }
+          }],
+        },
+        legend: {
+          display: false
+        }
+      }
+    });
+    var ctx = document.getElementById("maxspeed");
+    var myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: weatherdate,
+        datasets: [{
+          label: "Sessions",
+          lineTension: 0.3,
+          backgroundColor: "rgba(2,117,216,0.2)",
+          borderColor: "rgba(2,117,216,1)",
+          pointRadius: 5,
+          pointBackgroundColor: "rgba(2,117,216,1)",
+          pointBorderColor: "rgba(255,255,255,0.8)",
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(2,117,216,1)",
+          pointHitRadius: 50,
+          pointBorderWidth: 2,
+          data: maxspeed
+        }],
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            time: {
+              unit: 'date'
+            },
+            gridLines: {
+              display: false
+            },
+            ticks: {
+              maxTicksLimit: 20
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: 10,
+              maxTicksLimit: 10
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, .125)",
+            }
+          }],
+        },
+        legend: {
+          display: false
+        }
+      }
+    });
+    var ctx = document.getElementById("precip");
+    var myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: weatherdate,
+        datasets: [{
+          label: "Sessions",
+          lineTension: 0.3,
+          backgroundColor: "rgba(2,117,216,0.2)",
+          borderColor: "rgba(2,117,216,1)",
+          pointRadius: 5,
+          pointBackgroundColor: "rgba(2,117,216,1)",
+          pointBorderColor: "rgba(255,255,255,0.8)",
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(2,117,216,1)",
+          pointHitRadius: 50,
+          pointBorderWidth: 2,
+          data: percp
+        }],
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            time: {
+              unit: 'date'
+            },
+            gridLines: {
+              display: false
+            },
+            ticks: {
+              maxTicksLimit: 20
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: 70,
+              maxTicksLimit: 10
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, .125)",
+            }
+          }],
+        },
+        legend: {
+          display: false
+        }
+      }
+    });
+  }
---
+++ static/js/assets/demo/chart-line.js
@@ -0,0 +1,4 @@
+d3.csv('C:\Users\goyal\Downloads\Farmer App - Weather data from MetSwift-20210603T125907Z-001\Farmer App - Weather data from MetSwift\Not For Profit - Punjab\Station Data\Blended\IN0008B.csv')
+  .then(makeChart);
+
+function makeChart(weather)
